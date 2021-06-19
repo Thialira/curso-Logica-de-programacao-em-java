a6252b4450472313e29a1f6c215602be3526c919
Thiago Jefferson Cordeiro Mendes de Lira <tjlira07@gmail.com>
2021-06-18 21:55:08 -0300

    Estrutura sequencial

---
+++ uri1009/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		double salariofixo, vendas, Total;
+		
+		nome = sc.next();
+		salariofixo = sc.nextDouble();
+		vendas = sc.nextDouble();
+		
+		Total = salariofixo + vendas * 0.15;
+		
+		System.out.printf("TOTAL = R$ %.2f%n",Total);		
+		
+		sc.close();
+
+	}
+
+}
